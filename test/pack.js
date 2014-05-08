b2a4338ff32f1b595f1e00753b0cd2fead01aa6f
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-08 11:33:14 +1000

    Test float packing

--- test/pack.js
+++ test/pack.js
@@ -10,3 +10,18 @@ test('can pack a negative integer', function(t) {
   t.plan(1);
   t.equal(lexi(-1), 'N0000000000000001', 'ok');
 });
+
+test('can pack a float', function(t) {
+  t.plan(1);
+  t.equal(lexi(5.23423), 'P0000000000000005', 'ok');
+});
+
+test('can pack a negative float', function(t) {
+  t.plan(1);
+  t.equal(lexi(-10.35), 'N0000000000000010', 'ok');
+});
+
+test('attempting to pack a string returns the original string', function(t) {
+  t.plan(1);
+  t.equal(lexi('hi'), 'hi', 'value unchanged');
+});
